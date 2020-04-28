bba2fcc8ddaf0e95a1d98ee2bb0aef610fc07d10
Sam Sverko <sam.sverko@gmail.com>
2020-04-28 14:29:15 -0400

    Insert new trivia and lobby controller - Create route with validation and sanitization.

--- api/controller.js
+++ api/controller.js
@@ -1,7 +1,76 @@
 // dependencies
 require('dotenv').config()
+const DB_COLLECTION_TRIVIA = process.env.DB_COLLECTION_TRIVIA
+const DB_COLLECTION_LOBBIES = process.env.DB_COLLECTION_LOBBIES
 
 module.exports = {
+  insertTriviaAndLobby: async (req, res, next) => {
+    // retrieve all existing triviaIds
+    req.app.db.collection(DB_COLLECTION_TRIVIA).find(
+      {},
+      {
+        projection: {
+          _id: 0,
+          createdAt: 0,
+          host: 0,
+          rounds: 0,
+          tieBreaker: 0
+        }
+      })
+      .toArray((error, documents) => {
+        if (error) {
+          res.send(error)
+        } else {
+          const existingTriviaIds = []
+          documents.forEach((document) => {
+            existingTriviaIds.push(document.triviaId)
+          })
+          // generate a triviaId that does not match any already-existing triviaIds
+          let newTriviaId = ''
+          const availableCharacters = 'abcdefghijklmnopqrstuvwxyz'
+          do {
+            // newTriviaId = ''
+            for (let i = 0; i < 4; i++) {
+              newTriviaId += availableCharacters.charAt(Math.floor(Math.random() * availableCharacters.length))
+            }
+          } while (existingTriviaIds.includes(newTriviaId))
+          // insert new trivia document
+          req.app.db.collection(DB_COLLECTION_TRIVIA).insertOne(
+            {
+              createdAt: new Date().toISOString(),
+              triviaId: newTriviaId.toLowerCase(),
+              host: req.body.name.toLowerCase(),
+              rounds: [],
+              tieBreaker: {}
+            },
+            (error, triviaResult) => {
+              if (error) {
+                res.send(error)
+              } else {
+                // insert associated lobby document
+                req.app.db.collection(DB_COLLECTION_LOBBIES).insertOne(
+                  {
+                    createdAt: new Date().toISOString(),
+                    triviaId: newTriviaId.toLowerCase(),
+                    host: req.body.name.toLowerCase(),
+                    players: [],
+                    responses: []
+                  },
+                  (error, lobbiesResult) => {
+                    if (error) {
+                      res.send(error)
+                    } else {
+                      // return created trivia and lobby documents
+                      res.send([triviaResult.ops[0], lobbiesResult.ops[0]])
+                    }
+                  }
+                )
+              }
+            }
+          )
+        }
+      })
+  },
   getDocument: async (req, res, next) => {
     req.app.db.collection(req.params.collection).findOne(
       { triviaId: req.params.triviaId },
@@ -19,8 +88,8 @@ module.exports = {
           next(error)
         } else {
           let document = result
-          if (req.params.collection === process.env.DB_COLLECTION_TRIVIA) {
-          } else if (req.params.collection === process.env.DB_COLLECTION_LOBBIES) {
+          if (req.params.collection === DB_COLLECTION_TRIVIA) {
+          } else if (req.params.collection === DB_COLLECTION_LOBBIES) {
             // tieBreaker responses || specified responses from specified round || all responses from specified round || specified responses from all rounds
             if (typeof req.query.tieBreaker !== 'undefined') {
               const filteredReponses = []
--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,11 @@
 // dependencies
 require('dotenv').config()
 const API_VERSION = process.env.API_VERSION || 1
+const DB_COLLECTION_TRIVIA = process.env.DB_COLLECTION_TRIVIA
+const DB_COLLECTION_LOBBIES = process.env.DB_COLLECTION_LOBBIES
 const express = require('express')
 const router = express.Router()
-const { param, query, validationResult } = require('express-validator')
+const { body, param, query, validationResult } = require('express-validator')
 
 // local files
 const apiController = require('./controller')
@@ -16,7 +18,7 @@ router.get('/', (req, res) => {
 })
 
 router.get(`/api/v${API_VERSION}/:collection/:triviaId`, [
-  param('collection').isString().trim().escape().isIn([process.env.DB_COLLECTION_TRIVIA, process.env.DB_COLLECTION_LOBBIES]),
+  param('collection').isString().trim().escape().isIn([DB_COLLECTION_TRIVIA, DB_COLLECTION_LOBBIES]),
   param('triviaId').isString().trim().escape().isLength({ min: 4, max: 4 }),
   query('roundNumber').trim().escape().toInt().not().isIn([NaN]).optional(),
   query('questionNumber').trim().escape().toInt().not().isIn([NaN]).optional(),
@@ -29,13 +31,20 @@ router.get(`/api/v${API_VERSION}/:collection/:triviaId`, [
     validationResult(req).throw()
     apiController.getDocument(req, res, next)
   } catch (validationError) {
-    const error = new Error()
-    error.statusCode = 422
-    error.title = 'API parameter validation failed.'
-    error.method = req.method
-    error.location = req.url
-    error.details = validationError.errors
-    return next(error)
+    handleValidationError(next, 422, 'API parameter validation failed.', req.method, req.url, validationError.errors)
+  }
+})
+
+router.post(`/api/v${API_VERSION}/${DB_COLLECTION_TRIVIA}/new`, [
+  body('name').isString().trim().escape().matches(/^[a-z0-9]+$/, 'i').isLength({ min: 3, max: 10 })
+], (req, res, next) => {
+  console.log(`${req.method} request for ${req.url}.`)
+
+  try {
+    validationResult(req).throw()
+    apiController.insertTriviaAndLobby(req, res, next)
+  } catch (validationError) {
+    handleValidationError(next, 422, 'API parameter validation failed.', req.method, req.url, validationError.errors)
   }
 })
 
@@ -45,5 +54,16 @@ router.get('*', (req, res) => {
 
   res.send('404')
 })
+
+// handle express-validator validation failure
+function handleValidationError (next, statusCode, title, method, location, detail) {
+  const error = new Error()
+  error.statusCode = statusCode
+  error.title = title
+  error.method = method
+  error.location = location
+  error.details = detail
+  return next(error)
+}
 
 module.exports = router
