5864a237d04e85c6839939021e581274ff9ded83
Sam Sverko <sam.sverko@gmail.com>
2020-05-22 13:23:50 -0400

    Server debug logging

--- api/controller.js
+++ api/controller.js
@@ -257,6 +257,7 @@ module.exports = {
           if (error) {
             utils.handleServerError(next, 502, 'Database query failed.', req.method, req.url, '\'joinLobby() updateOne\' query failed.')
           } else {
+            console.log('OK JOIN LOBBY')
             res.sendStatus(200)
           }
         }
